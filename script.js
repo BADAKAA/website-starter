3c0f7764bf7477f4d47ab47912da8bd0a132eb6b
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2021-05-24 22:40:22 +0200

    fix typo

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 
 	console.log("%cManual:", "color:darkred; font-weight:bold;font-family:'PT Sans', sans-serif");
 	console.log("%cIf you want to use TypeScript, write your code into the file: './src/index.ts'. \n(A node.js installation is required.)\nType 'npm i' into your editor's terminal if this is your first time opening this folder. Then, type 'npm run watch' into the terminal every time you open this project in your editor.\nIf you simply want to use JavaScript, use the file './script.js' file \nIn that case, you can delete the files './package-lock.json', './package.json', './tsconfig.json', './rollup.config.js' and the directory: '/src'.", "color:darkgreen;font-family:'PT Sans', sans-serif");
-	console.log("%cThe case is similar for css. If you want to use scss, write your text into the file: './style/style.scss'.\n(An scss-compiler is required.)\nIf you want to use regular css, simply delte the directory './style' and use the file './style.css'.", "color:darkgreen;font-family:'PT Sans', sans-serif");
+	console.log("%cThe case is similar for css. If you want to use scss, write your text into the file: './style/style.scss'.\n(An scss-compiler is required.)\nIf you want to use regular css, simply delete the directory './style' and use the file './style.css'.", "color:darkgreen;font-family:'PT Sans', sans-serif");
 
 }());
